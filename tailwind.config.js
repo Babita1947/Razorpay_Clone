6b33f55b4a1f947994b71b6490ee8154ceaecf23
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-10-08 22:13:51 +0530

    remaing code uploaded

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,12 @@ module.exports = {
     lightBlue: "#2b84ea",
     lightBlue300: "#4b94ed",
     lightBlue500: "#0b72e7",
-    greenLight: "#818597",
+    greenLight: "#61cea6",
     lightGray: "#e2e2e2",
     grayBlue: "#344a6c",
     deepBlueHead: "#162f56",
     gray2:"#525a76",
+    grayText:"#6D6D6D",
   },},
 
   },
